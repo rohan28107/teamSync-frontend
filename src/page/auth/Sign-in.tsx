9315b8f2f6b4b9911112771974c823df0d9b1846
Rohan Gajare <rohangajre21@gmail.com>
2025-07-27 17:44:06 +0530

    removing google login for now

--- src/page/auth/Sign-in.tsx
+++ src/page/auth/Sign-in.tsx
@@ -87,21 +87,22 @@ const SignIn = () => {
             <CardHeader className="text-center">
               <CardTitle className="text-xl">Welcome back</CardTitle>
               <CardDescription>
-                Login with your Email or Google account
+                Login with your Email 
+                {/* or Google account */}
               </CardDescription>
             </CardHeader>
             <CardContent>
               <Form {...form}>
                 <form onSubmit={form.handleSubmit(onSubmit)}>
                   <div className="grid gap-6">
-                    <div className="flex flex-col gap-4">
+                    {/* <div className="flex flex-col gap-4">
                       <GoogleOauthButton label="Login" />
                     </div>
                     <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
                       <span className="relative z-10 bg-background px-2 text-muted-foreground">
                         Or continue with
                       </span>
-                    </div>
+                    </div> */}
                     <div className="grid gap-3">
                       <div className="grid gap-2">
                         <FormField
